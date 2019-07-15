e7b329c9139d1c43cb5a040a45ce291e84ad7e4b
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2019-07-15 11:14:11 +0530

    Update Sum of Digits.java

--- Sum of Digits.java
+++ Sum of Digits.java
@@ -1,1 +1,18 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String num = sc.next();
+	    char a[] = num.toCharArray();
+	    //System.out.println(a[0]);
+	    int sum = 0;
+	    for(int i = 0; i < a.length; i++)
+	    {
+	        sum += (int) a[i] - 48;
+	    }
+	    System.out.println(sum);
+		
+	}
+}
 
