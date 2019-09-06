73128df48bb4faf033ae8088f1086d6b69dfad43
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2019-09-06 08:39:12 +0530

    Create inc.java

---
+++ inc.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class sequence {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i =  0; i < n; i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		int inc[] = new int[n];
+		for(int i = 0; i < n - 1; i++)
+		{
+			int count = 0;
+			int maxi = a[i];
+			for(int j = i+1; j < n; j++ )
+			{
+				if (a[j] > a[i])
+				{
+					count++;
+				}
+				else
+				{
+					break;
+				}
+			}
+			inc[i] = count;
+		}
+		int max = -1;
+		int index = -1;
+		for(int i = 0; i < n; i++)
+		{
+			//System.out.println(inc[i]);
+			if (max < inc[i])
+			{
+				max = inc[i];
+				index = i;
+			}
+		}
+		System.out.println("Count = "+max+" start index = "+index);
+		for(int i = index; i <= max + index; i++ )
+		{
+			System.out.println(a[i]);
+		}
+		
+
+	}
+
+}
